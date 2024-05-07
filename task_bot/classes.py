16a90e733328eb0ce9a8c397fd2baaba44100805
julia <julia.zhyvylo@asila.mk>
2024-05-07 20:55:33 +0200

    added basic code for hw

---
+++ task_bot/classes.py
@@ -0,0 +1,30 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    # реалізація класу
+		pass
+
+class Phone(Field):
+    # реалізація класу
+		pass
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    # реалізація класу
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    # реалізація класу
+		pass
